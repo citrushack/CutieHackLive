2ec5293c7bb1fc16dfc2cf5f386de5cd907333b3
RJ <44298360+r-jo@users.noreply.github.com>
2020-11-07 06:28:24 -0800

    Cutiehack is live

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-var endDate = new Date("Nov 7, 2020 07:00:00").getTime();
+var endDate = new Date("Nov 7, 2020 20:00:00").getTime();
 
 var timer = setInterval(function () {
   let now = new Date().getTime();
   let t = endDate - now;
   if (t >= 0) {
-    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    
+    // let days = Math.floor(t / (1000 * 60 * 60 * 24));
     let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let secs = Math.floor((t % (1000 * 60)) / 1000);
-    document.getElementById("timer-days").innerHTML =
-      days + "<span class='label'> Days</span>";
+    // document.getElementById("timer-days").innerHTML =
+    //   days + "<span class='label'> Days</span>";
 
     document.getElementById("timer-hours").innerHTML =
       ("0" + hours).slice(-2) + "<span class='label'> Hours</span>";
@@ -18,8 +19,8 @@ var timer = setInterval(function () {
       ("0" + mins).slice(-2) + "<span class='label'> Minutes</span>";
 
     document.getElementById("timer-secs").innerHTML =
-      ("0" + secs).slice(-2) + "<span class='label'> Sec</span>";
+      ("0" + secs).slice(-2) + "<span class='label'> Seconds</span>";
   } else {
-    document.getElementById("timer").innerHTML = "Cutie Hack is Live!";
+    document.getElementById("timer").innerHTML = "Cutie Hack is Over!";
   }
 }, 1000);
