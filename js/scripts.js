4e5eb0983df598265b8cd0c3d4e766b4f1279352
r-jo <rdrsjck@gmail.com>
2020-11-06 18:08:14 -0800

    Fixed countdown

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var endDate = new Date("Nov 8, 2020 12:00:00").getTime();
+var endDate = new Date("Nov 7, 2020 20:00:00").getTime();
 
 var timer = setInterval(function () {
   let now = new Date().getTime();
